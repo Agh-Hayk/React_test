dc6d189f30b4a9d04bf19860e230a4321dabf672
A_Hayk <agh.hayk97@mail.ru>
2020-12-17 20:55:08 +0400

    New Components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <div style={{marginTop:'20px', border:'1px solid #ccc', padding:'20px'}}>
+            <p>Component 3</p>
+        </div>
+    )
+}
+
+export default Footer
