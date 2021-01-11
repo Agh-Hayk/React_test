ef2159d51f5d904d3764979bc93ec4045c00b9da
Hayk <agh.hayk97@mail.ru>
2021-01-11 13:39:48 +0400

    homework-11-update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-        {/**<h2>Homework 5</h2>
-        <Header />
-        <Section />
-        <Footer />
-        <h2>Homework 6,7</h2>
-        <Product name="BMW 745e" price="950$" description="The standard features of the BMW 745e xDrive iPerformance include 3.0L I-6 389hp hybrid gas intercooled turbo engine, 8-speed automatic transmission with overdrive..." />
-        <h2>Homework 8,9</h2>**/}
         <ToDo />
     </div>
   );
